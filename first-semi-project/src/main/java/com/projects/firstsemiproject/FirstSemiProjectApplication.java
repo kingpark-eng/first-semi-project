d8675c4055c585ecf45d07dace1927dcd7a8e887
qkr12294 <qkr12294@gmail.com>
2024-05-08 14:34:43 +0900

    first commit

---
+++ first-semi-project/src/main/java/com/projects/firstsemiproject/FirstSemiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.projects.firstsemiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSemiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSemiProjectApplication.class, args);
+	}
+
+}
---
+++ first-semi-project/src/test/java/com/projects/firstsemiproject/FirstSemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.firstsemiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
